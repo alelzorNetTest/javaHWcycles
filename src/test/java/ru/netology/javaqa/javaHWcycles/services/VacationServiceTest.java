f68da86dea225bd952839194cd8d27a9290d278f
bigrusal <bigrusal@yandex.ru>
2023-04-27 09:57:00 +1000

    task_2

--- src/test/java/ru/netology/javaqa/javaHWcycles/services/VacationServiceTest.java
+++ src/test/java/ru/netology/javaqa/javaHWcycles/services/VacationServiceTest.java
@@ -2,30 +2,21 @@ package ru.netology.javaqa.javaHWcycles.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationServiceTest {
 
-    @Test
-    public void shouldCalculateVacationLength1() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/vacation.csv")
+    public void shouldCalculateVacationLength(int expected, int income, int expenses, int threshold) {
         VacationService service = new VacationService();
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
+        //int income = 10_000;
+        //int expenses = 3_000;
+        //int threshold = 20_000;
 
-        int expected = 3;
-        int actual = service.calculate(income, expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalculateVacationLength2() {
-        VacationService service = new VacationService();
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-
-        int expected = 2;
+        //int expected = 3;
         int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
